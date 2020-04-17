cfb1549184d1a223ad830a8b97ab6065ef82de16
ouyanglinxia <ouyanglinxia@xiaomi.com>
2020-04-17 11:38:27 +0800

    setp_pt.sh

---
+++ fast-shell/setp_pt.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+echo "start deploy";
+
+npm install;
+
+LAST_BRANCH=$GIT_BRANCH;
+LAST_COMMIT=$GIT_COMMIT;
+
+# BRANCH有值表示是手动参数化构建
+if [ $BRANCH == "dev" ]
+then
+	echo "in branch dev";
+    git checkout dev;
+    git pull --rebase;
+    LAST_BRANCH="origin/dev";
+    LAST_COMMIT=$(git rev-parse HEAD);
+fi
+
+if [ $BRANCH == "master" ]
+then
+	echo "in branch master";
+    git checkout master;
+    git pull --rebase;
+    LAST_BRANCH="origin/master";
+    LAST_COMMIT=$(git rev-parse HEAD);
+fi
+
+echo $LAST_COMMIT;
+echo $LAST_BRANCH;
+
+node deploy.js $LAST_COMMIT $LAST_BRANCH;
